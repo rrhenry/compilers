5a3634978ed56932cad12f8bee4f30ce1089d2b8
Alexi <ay_turcotte@laurentian.ca>
2014-09-23 15:36:59 -0400

    A few changes.

--- scanner.c
+++ scanner.c
@@ -1,23 +1,28 @@
-enum { 	module, 
-		ident, letter, digit,
-	 	importList, import,
-	 	declarationSequence, constantDeclaration,
-	 	identdef,
-	 	constExpression, expression, simpleExpression,
-	 	term, factor, number, integer, hexDigit, real, 
-	 	scaleFactor, charConstant, string,
-	 	set, element, 
-	 	designator, expList, actualParameters,
-	 	mulOperator, addOperator, relation, 
-	 	typeDeclaration, type, qualident, arrayType, length, recordType, baseType,
-	 	variableDeclaration, procedureDeclaration, procedureHeading, formalParameters,
-	 	fpsSection, formalType, prodecureBody, forwardDeclaration,
-	 	statementSequence, statement, assignment, procedureCall, 
-	 	ifStatement, caseStatement, _case, caseLabelList, caseLabels,
-	 	whileStatement, repeatStatement, loopStatement, withStatement 
-	 };
+enum Token  { 	
+			module, 
+			ident, letter, digit,
+		 	importList, import,
+		 	declarationSequence, constantDeclaration,
+		 	identdef,
+		 	constExpression, expression, simpleExpression,
+		 	term, factor, number, integer, hexDigit, real, 
+		 	scaleFactor, charConstant, string,
+		 	set, element, 
+		 	designator, expList, actualParameters,
+		 	mulOperator, addOperator, relation, 
+		 	typeDeclaration, type, qualident, arrayType, length, recordType, baseType,
+		 	variableDeclaration, procedureDeclaration, procedureHeading, formalParameters,
+		 	fpsSection, formalType, prodecureBody, forwardDeclaration,
+		 	statementSequence, statement, assignment, procedureCall, 
+		 	ifStatement, caseStatement, _case, caseLabelList, caseLabels,
+		 	whileStatement, repeatStatement, loopStatement, withStatement 
+		    };
+
+Token curTok;
+char curChar;
 
 int main()
-{
+{	
+	// so cleeeeaaaaaan
 	return 42;
 }
