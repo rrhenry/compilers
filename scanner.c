99578d3812980618951e2446c49aeb577587552f
Alexi <ay_turcotte@laurentian.ca>
2014-09-23 15:51:29 -0400

    It wasn't compiling cause I'm silly.

--- scanner.c
+++ scanner.c
@@ -1,22 +1,22 @@
-enum Token  { 	
-			module, 
-			ident, letter, digit,
-		 	importList, import,
-		 	declarationSequence, constantDeclaration,
-		 	identdef,
-		 	constExpression, expression, simpleExpression,
-		 	term, factor, number, integer, hexDigit, real, 
-		 	scaleFactor, charConstant, string,
-		 	set, element, 
-		 	designator, expList, actualParameters,
-		 	mulOperator, addOperator, relation, 
-		 	typeDeclaration, type, qualident, arrayType, length, recordType, baseType,
-		 	variableDeclaration, procedureDeclaration, procedureHeading, formalParameters,
-		 	fpsSection, formalType, prodecureBody, forwardDeclaration,
-		 	statementSequence, statement, assignment, procedureCall, 
-		 	ifStatement, caseStatement, _case, caseLabelList, caseLabels,
-		 	whileStatement, repeatStatement, loopStatement, withStatement 
-		    };
+typedef enum { 	
+				module, 
+				ident, letter, digit,
+			 	importList, import,
+			 	declarationSequence, constantDeclaration,
+			 	identdef,
+			 	constExpression, expression, simpleExpression,
+			 	term, factor, number, integer, hexDigit, real, 
+			 	scaleFactor, charConstant, string,
+			 	set, element, 
+			 	designator, expList, actualParameters,
+			 	mulOperator, addOperator, relation, 
+			 	typeDeclaration, type, qualident, arrayType, length, recordType, baseType,
+			 	variableDeclaration, procedureDeclaration, procedureHeading, formalParameters,
+			 	fpsSection, formalType, prodecureBody, forwardDeclaration,
+			 	statementSequence, statement, assignment, procedureCall, 
+			 	ifStatement, caseStatement, _case, caseLabelList, caseLabels,
+			 	whileStatement, repeatStatement, loopStatement, withStatement 
+			} Token;
 
 Token currTok;
 char currChar;
