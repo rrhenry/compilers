9e5707ed2a663bb62d5b11f2b3c4d433bc38b844
Alexi <ay_turcotte@laurentian.ca>
2014-09-23 15:26:56 -0400

    Committing the first attempt at a scanner for Oberon 2 (we think).

---
+++ scanner.c
@@ -0,0 +1,23 @@
+enum { 	module, 
+		ident, letter, digit,
+	 	importList, import,
+	 	declarationSequence, constantDeclaration,
+	 	identdef,
+	 	constExpression, expression, simpleExpression,
+	 	term, factor, number, integer, hexDigit, real, 
+	 	scaleFactor, charConstant, string,
+	 	set, element, 
+	 	designator, expList, actualParameters,
+	 	mulOperator, addOperator, relation, 
+	 	typeDeclaration, type, qualident, arrayType, length, recordType, baseType,
+	 	variableDeclaration, procedureDeclaration, procedureHeading, formalParameters,
+	 	fpsSection, formalType, prodecureBody, forwardDeclaration,
+	 	statementSequence, statement, assignment, procedureCall, 
+	 	ifStatement, caseStatement, _case, caseLabelList, caseLabels,
+	 	whileStatement, repeatStatement, loopStatement, withStatement 
+	 };
+
+int main()
+{
+	return 42;
+}
