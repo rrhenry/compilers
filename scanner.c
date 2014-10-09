9b88072120869e3e0edb766f4dee5ab2fe246307
Roxanne <rrhenry@laurentian.ca>
2014-10-09 00:00:40 -0400

    Added a few things: getChar functionality, charType, and setting currLine to incoming line

--- scanner.c
+++ scanner.c
@@ -27,6 +27,7 @@ char currChar;
 char currLine [80];		// 80 character limit is arbitrary but sensible.
 								// should be pointer...?
 int count = 0;
+Token currObj;
 FILE *toScan;
 
 //	*
@@ -35,12 +36,17 @@ FILE *toScan;
 //	-- might want to grab from currLine and put into currChar?
 //	++ probably not though. We need to fetch a character based on a counter, right?
 //  ++ we don't necessarily want that function in getLine? 
+//
 void getChar()
 {
 	if(count >= 80) //ok i'm assuming currline's size is 80 characters total
 		count = 0; 	//starting at index 0, and will be over limit at or abover 80
+	//this is sorta shit implementation, but i don't have a clear idea what will be 
+	//calling this yet, so for now, it will collect all the characters as it gets 
+	//called and when it reaches 80, it'll restart, so better hope we have a new line 
+	//by then.
 
-	currChar = currline[count]; 
+	currChar = currLine[count]; 
 }
 
 //	*
@@ -49,8 +55,24 @@ void getChar()
 void getLine()
 {
 	char *funLine = (char *) malloc(BUFF_SIZE);
-	if ( fgets(funLine, BUFF_SIZE, toScan) != NULL )
+	if ( fgets(funLine, BUFF_SIZE, toScan) != NULL ){
 		fputs(funLine, stdout);
+		*currLine = *funLine;
+	}
+
+}
+
+// *
+// Determines if currChar is a digit (0...9) or a letter (a...z | A...Z)
+// sets currObj to relevant state
+// -- might merge this with getChar because who wants to call this everytime
+//
+void charType()
+{
+	if(currChar >= 0 && currChar <= 9)
+		currObj = digit;
+	else if ( (currChar >= 'a' && currChar <= 'z') | currChar >= 'A' && currChar <= 'Z')
+		currObj = letter;
 
 }
 
