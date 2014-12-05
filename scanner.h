ece76f5193a79fd2bafad73401a36f2418e521f5
Roxanne <rrhenry@laurentian.ca>
2014-12-05 14:16:47 -0500

    This is totally the header fiel...

---
+++ scanner.h
@@ -0,0 +1,20 @@
+// scanner.c header file 
+// Alexi Turcotte
+// Roxanne Henry
+
+#ifndef SCANNER_H
+#define SCANNER_H
+
+void qualident();
+void type();
+void FormParams();
+void ImportList();
+void StrucType();
+void DeclSeq();
+void Module();
+void ProcDecl();
+void StatSeq();
+void expr();
+void stat();
+
+#endif
