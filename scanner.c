3ea9218ddcbe7ef6bba9d817b93478cc423ec088
Alexi <ay_turcotte@laurentian.ca>
2014-12-15 13:05:12 -0500

    More code gen: nearly done initialization of the compiler portion of the code

--- scanner.c
+++ scanner.c
@@ -4,7 +4,7 @@
 
 /*			TODO LIST
 		-> phase out need for <string.h>
-		-> make fatal errors fatal
+		-> make fatal errors fatal (i.e. ctrl-find F_ERROR and add exit statement)
 */
 
 #include <stdio.h>		// need for file io
@@ -85,15 +85,40 @@ Token setTok;
 
 FILE *toScan;
 
+/* BEGIN: String processing methods */
+
+char *strcopy(char *dst, const char *src)
+{
+   char *saved = dst;
+   while (*src)
+   {
+       *dst++ = *src++;
+   }
+   *dst = 0;
+   return saved;
+}
+
+/* END:   String processing methods */
+
 /* Code Generator Data Fields */
 
 int currlev = 0;
 int lc = 0;
-int stptr = 0;
+int stptr = 0;				// ptr to last entry in symbol table
+int ttptr = 0;				// ptr to last entry in type table
 const int maxlev = 10;
 const int stsize = 256;		// length of symbol table
 const int ttsize = 128;  	// length of type table
 const int codemax = 1023;   // length of code array
+const int idbuffsize = 16;	// length of identifiers
+
+const char *mnemonic [16][5];
+
+/* Predefined types */
+
+int inttyp, realtyp, booltyp, chartyp, texttyp;
+
+/*                  */
 
 typedef enum 				// classes of identifiers
 {
@@ -103,6 +128,7 @@ typedef enum 				// classes of identifiers
 	,varcls
 	,constcls
 	,proccls
+	,stdpcls
 } IdClass;
 
 typedef enum				// types of types
@@ -111,6 +137,7 @@ typedef enum				// types of types
 	,recrodfrm
 	,ptrfrm
 	,procfrm
+	,scalarfrm
 } TypeForm;
 
 typedef enum				// all possible opcodes
@@ -133,30 +160,30 @@ typedef enum				// all possible opcodes
 	,nop
 } Opcode;
 
-struct vminstr
+struct vminstr				// struct for VM instructions
 {
 	Opcode op;				// opcode
 	int ld;					// static level difference
 	int ad;					// offset from beginning of stack frame
 };
 
-struct typerec
+struct typerec				// type struct
 {
 	int size;				// memory footprint
-	TypeForm type;			// type type
+	TypeForm form;			// type type
 };
 
 struct identrec 			// ident struct
 {
-	char *name [32];
-	int previd;
-	int idlev;
-	int idtyp;
-	IdClass class;
+	char name [16];			// name of the ident
+	int previd;				// link to previous identrec
+	int idlev;				// static level of the ident decl
+	int idtyp;				// type of the ident
+	IdClass class;			// class of the ident (i.e. ref to class table)
 	// TODO: need to store class information
 };
 
-struct identrec symtab [256];  	// symbol table
+struct identrec symtab [256];  		// symbol table
 struct typerec typetab [128];		// type table
 struct vminstr code    [1023];		// generated code
 int scopetab [10];					// scope table
@@ -164,6 +191,8 @@ int scopetab [10];					// scope table
 /* End Code Gen Data Fields */
 
 /* BEGIN: Code gen methods */
+
+void enterstdtypes();
 
 void enterScope()
 {
@@ -201,7 +230,7 @@ void genCode( Opcode o, int l, int a)
 void enterstdident ( char id [], IdClass cls, int ttp)
 {
 	stptr ++;
-//	symtab[ stptr].name = id;
+	strcopy(symtab[ stptr].name, id);
 	symtab[ stptr].previd = scopetab[ currlev];
 	symtab[ stptr].idlev = currlev;
 	symtab[ stptr].idtyp = ttp;
@@ -209,22 +238,157 @@ void enterstdident ( char id [], IdClass cls, int ttp)
 	scopetab[ currlev] = stptr;
 }
 
-void searchid( char id [32])
+void searchid( char id [16])
 {
-//	int stp = stptr;			// local symbol table ptr
-//	int lev = 0;				// local var for level
-//	symtab[ 0].name = id;		// sentinel for search
+	int stp = stptr;				// local symbol table ptr
+	int lev = 0;					// local var for level
+	strcopy(symtab[ 0].name, id);	// sentinel for search
 
-//	lev = currlev;				// start searching at the current scope level
-//	do
-//	{
-//		stp = scopetab[ lev];
-//		while ( strcmp(symtab[ stp].name, id) != 0)
-//		{
-//			stp = symtab[ stp].previd;
-//		}
-//	}
+	lev = currlev;					// start searching at the current scope level
+	do
+	{
+		stp = scopetab[ lev];
+		while ( strcmp(symtab[ stp].name, id) != 0)
+		{
+			stp = symtab[ stp].previd;
+		}
+	} while (stp == 0 || lev >= 0);
+
+	if (stp == 0)
+		fputs("ERROR: 42 Undeclared identifier.\n", stdout);
 }
+
+void insertid( char id [16], IdClass cls)
+{
+	int stp = scopetab[ currlev];
+	strcopy(symtab[ 0].name, id);	// sentinel for search
+
+	while ( strcmp(symtab[ stp].name, id) != 0)
+	{
+		stp = symtab[ stp].previd;
+	}
+	if ( stp != 0)
+	{
+		fputs("ERROR: 44 Multiple declarations.\n", stdout);
+	}
+	if ( stptr < stsize)
+	{
+		stptr ++;
+	}
+	else
+	{
+		fputs("F_ERROR: Symbol table overflow.\n", stdout);
+	}
+
+	strcopy(symtab[ stptr].name, id);
+	symtab[ stptr].class = cls;
+	symtab[ stptr].idlev = currlev;
+	symtab[ stptr].previd = scopetab[ currlev];
+
+	scopetab[ currlev] = stptr;
+
+}
+
+//* BEGIN: Subset of Code Gen methods -- compiler initialization *//
+
+void initstdmnemonics()
+{
+    mnemonic[ opr ][ 0] = "opr ";
+    mnemonic[ push][ 0] = "push";
+    mnemonic[ pshc][ 0] = "pshc";
+    mnemonic[ psha][ 0] = "psha";
+    mnemonic[ pshi][ 0] = "pshi";
+    mnemonic[ pop ][ 0] = "pop ";
+    mnemonic[ popi][ 0] = "popi";
+    mnemonic[ jsr ][ 0] = "jsr ";
+    mnemonic[ isp ][ 0] = "isp ";
+    mnemonic[ jmp ][ 0] = "jmp ";
+    mnemonic[ jmpc][ 0] = "jmpc";
+    mnemonic[ jmpx][ 0] = "jmpx";
+    mnemonic[ for0][ 0] = "for0";
+    mnemonic[ for1][ 0] = "for1";
+    mnemonic[ selc][ 0] = "selc";
+    mnemonic[ nop ][ 0] = "nop ";
+}
+
+void initstdidents()
+{
+	enterstdtypes();
+
+	int nstdidents = 24;
+	char *stdidents [24][16];
+	stdidents[  0][ 0] = "ABS            ";
+	stdidents[  1][ 0] = "CHAR           ";
+	stdidents[  2][ 0] = "FLT            ";
+	stdidents[  3][ 0] = "LSL            ";
+	stdidents[  4][ 0] = "REAL           ";
+	stdidents[  5][ 0] = "ASR            ";
+	stdidents[  6][ 0] = "CHR            ";
+	stdidents[  7][ 0] = "INC            ";
+	stdidents[  8][ 0] = "NEW            ";
+	stdidents[  9][ 0] = "ROR            ";
+	stdidents[ 10][ 0] = "ASSERT         ";
+	stdidents[ 11][ 0] = "DEC            ";
+	stdidents[ 12][ 0] = "INCL           ";
+	stdidents[ 13][ 0] = "ODD            ";
+	stdidents[ 14][ 0] = "SET            ";
+	stdidents[ 15][ 0] = "BOOLEAN        ";
+	stdidents[ 16][ 0] = "EXCL           ";
+	stdidents[ 17][ 0] = "INTEGER        ";
+	stdidents[ 18][ 0] = "ORD            ";
+	stdidents[ 19][ 0] = "UNPK           ";
+	stdidents[ 20][ 0] = "BYTE           ";
+	stdidents[ 21][ 0] = "FLOOR          ";
+	stdidents[ 22][ 0] = "LEN            ";
+	stdidents[ 23][ 0] = "PACK           ";
+
+	// enter std types into symbol table
+	enterstdident( stdidents[ 17][0], typcls, inttyp);		// integer
+	enterstdident( stdidents[  4][0], typcls, realtyp);		// real
+	enterstdident( stdidents[  1][0], typcls, chartyp);		// character
+	enterstdident( stdidents[ 15][0], typcls, booltyp);		// boolean
+
+	// enter std function calls
+	enterstdident( stdidents[  0][0], stdpcls, 0);			// ABS
+	enterstdident( stdidents[ 13][0], stdpcls, 0);			// ODD
+
+}
+
+
+// initialize standard types
+void enterstdtypes()
+{
+	// integer type
+	ttptr ++;
+	inttyp = ttptr;
+	typetab[ ttptr].size = 1;
+	typetab[ ttptr].form = scalarfrm;
+
+	// real type
+	ttptr ++;
+	realtyp = ttptr;
+	typetab[ ttptr].size = 1;
+	typetab[ ttptr].form = scalarfrm;
+
+	// boolean type
+	ttptr ++;
+	booltyp = ttptr;
+	typetab[ ttptr].size = 1;
+	typetab[ ttptr].form = scalarfrm;
+
+	// character type
+	ttptr ++;
+	chartyp = ttptr;
+	typetab[ ttptr].size = 1;
+	typetab[ ttptr].form = scalarfrm;
+}
+
+void initcompile()
+{
+	enterstdtypes();
+}
+
+//* END:   Subset of Code Gen methods -- compiler initialization *//
 
 /* END:   Code gen methods */
 
@@ -743,89 +907,89 @@ void nextSym()
 void initScanner()
 {
 	// Initialize Reserved Words
-	resWords [0][0] = "BOOLEAN";
-	resWords [1][0] = "CHAR";
-	resWords [2][0] = "FALSE";
-	resWords [3][0] = "NEW";
-	resWords [4][0] = "REAL";
-	resWords [5][0] = "TRUE";
-	resWords [6][0] = "ARRAY";
-	resWords [7][0] = "BEGIN";
-	resWords [8][0] = "BY";
-	resWords [9][0] = "CASE";
-	resWords [10][0] = "CONST";
-	resWords [11][0] = "DIV";
-	resWords [12][0] = "DO";
-	resWords [13][0] = "ELSE";
-	resWords [14][0] = "ELSIF";
-	resWords [15][0] = "END";
-	resWords [16][0] = "EXIT";
-	resWords [17][0] = "FOR";
-	resWords [18][0] = "IF";
-	resWords [19][0] = "IMPORT";
-	resWords [20][0] = "IN";
-	resWords [21][0] = "IS";
-	resWords [22][0] = "LOOP";
-	resWords [23][0] = "MOD";
-	resWords [24][0] = "MODULE";
-	resWords [25][0] = "NIL";
-	resWords [26][0] = "OF";
-	resWords [27][0] = "OR";
-	resWords [28][0] = "POINTER";
-	resWords [29][0] = "PROCEDURE";
-	resWords [30][0] = "RECORD";
-	resWords [31][0] = "REPEAT";
-	resWords [32][0] = "RETURN";
-	resWords [33][0] = "THEN";
-	resWords [34][0] = "TO";
-	resWords [35][0] = "TYPE";
-	resWords [36][0] = "UNTIL";
-	resWords [37][0] = "VAR";
-	resWords [38][0] = "WHILE";
-	resWords [39][0] = "WITH";
-//	resWords [40][0] = "INTEGER";
+	resWords [  0][ 0] = "BOOLEAN";
+	resWords [  1][ 0] = "CHAR";
+	resWords [  2][ 0] = "FALSE";
+	resWords [  3][ 0] = "NEW";
+	resWords [  4][ 0] = "REAL";
+	resWords [  5][ 0] = "TRUE";
+	resWords [  6][ 0] = "ARRAY";
+	resWords [  7][ 0] = "BEGIN";
+	resWords [  8][ 0] = "BY";
+	resWords [  9][ 0] = "CASE";
+	resWords [ 10][ 0] = "CONST";
+	resWords [ 11][ 0] = "DIV";
+	resWords [ 12][ 0] = "DO";
+	resWords [ 13][ 0] = "ELSE";
+	resWords [ 14][ 0] = "ELSIF";
+	resWords [ 15][ 0] = "END";
+	resWords [ 16][ 0] = "EXIT";
+	resWords [ 17][ 0] = "FOR";
+	resWords [ 18][ 0] = "IF";
+	resWords [ 19][ 0] = "IMPORT";
+	resWords [ 20][ 0] = "IN";
+	resWords [ 21][ 0] = "IS";
+	resWords [ 22][ 0] = "LOOP";
+	resWords [ 23][ 0] = "MOD";
+	resWords [ 24][ 0] = "MODULE";
+	resWords [ 25][ 0] = "NIL";
+	resWords [ 26][ 0] = "OF";
+	resWords [ 27][ 0] = "OR";
+	resWords [ 28][ 0] = "POINTER";
+	resWords [ 29][ 0] = "PROCEDURE";
+	resWords [ 30][ 0] = "RECORD";
+	resWords [ 31][ 0] = "REPEAT";
+	resWords [ 32][ 0] = "RETURN";
+	resWords [ 33][ 0] = "THEN";
+	resWords [ 34][ 0] = "TO";
+	resWords [ 35][ 0] = "TYPE";
+	resWords [ 36][ 0] = "UNTIL";
+	resWords [ 37][ 0] = "VAR";
+	resWords [ 38][ 0] = "WHILE";
+	resWords [ 39][ 0] = "WITH";
+//	resWords [ 40][ 0] = "INTEGER";
 
-	resWordTokens [0] = BOOLEAN_SYM;
-	resWordTokens [1] = CHAR_SYM;
-	resWordTokens [2] = FALSE_SYM;
-	resWordTokens [3] = NEW_SYM;
-	resWordTokens [4] = REAL_SYM;
-	resWordTokens [5] = TRUE_SYM;
-	resWordTokens [6] = ARRAY_SYM;
-	resWordTokens [7] = BEGIN_SYM;
-	resWordTokens [8] = BY_SYM;
-	resWordTokens [9] = CASE_SYM;
-	resWordTokens [10] = CONST_SYM;
-	resWordTokens [11] = DIV_SYM;
-	resWordTokens [12] = DO_SYM;
-	resWordTokens [13] = ELSE_SYM;
-	resWordTokens [14] = ELSIF_SYM;
-	resWordTokens [15] = END_SYM;
-	resWordTokens [16] = EXIT_SYM;
-	resWordTokens [17] = FOR_SYM;
-	resWordTokens [18] = IF_SYM;
-	resWordTokens [19] = IMPORT_SYM;
-	resWordTokens [20] = IN_SYM;
-	resWordTokens [21] = IS_SYM;
-	resWordTokens [22] = LOOP_SYM;
-	resWordTokens [23] = MOD_SYM;
-	resWordTokens [24] = MODULE_SYM;
-	resWordTokens [25] = NIL_SYM;
-	resWordTokens [26] = OF_SYM;
-	resWordTokens [27] = OR_SYM;
-	resWordTokens [28] = POINTER_SYM;
-	resWordTokens [29] = PROCEDURE_SYM;
-	resWordTokens [30] = RECORD_SYM;
-	resWordTokens [31] = REPEAT_SYM;
-	resWordTokens [32] = RETURN_SYM;
-	resWordTokens [33] = THEN_SYM;
-	resWordTokens [34] = TO_SYM;
-	resWordTokens [35] = TYPE_SYM;
-	resWordTokens [36] = UNTIL_SYM;
-	resWordTokens [37] = VAR_SYM;
-	resWordTokens [38] = WHILE_SYM;
-	resWordTokens [39] = WITH_SYM;
-//	resWordTokens [40] = INTEGER_SYM;
+	resWordTokens [  0] = BOOLEAN_SYM;
+	resWordTokens [  1] = CHAR_SYM;
+	resWordTokens [  2] = FALSE_SYM;
+	resWordTokens [  3] = NEW_SYM;
+	resWordTokens [  4] = REAL_SYM;
+	resWordTokens [  5] = TRUE_SYM;
+	resWordTokens [  6] = ARRAY_SYM;
+	resWordTokens [  7] = BEGIN_SYM;
+	resWordTokens [  8] = BY_SYM;
+	resWordTokens [  9] = CASE_SYM;
+	resWordTokens [ 10] = CONST_SYM;
+	resWordTokens [ 11] = DIV_SYM;
+	resWordTokens [ 12] = DO_SYM;
+	resWordTokens [ 13] = ELSE_SYM;
+	resWordTokens [ 14] = ELSIF_SYM;
+	resWordTokens [ 15] = END_SYM;
+	resWordTokens [ 16] = EXIT_SYM;
+	resWordTokens [ 17] = FOR_SYM;
+	resWordTokens [ 18] = IF_SYM;
+	resWordTokens [ 19] = IMPORT_SYM;
+	resWordTokens [ 20] = IN_SYM;
+	resWordTokens [ 21] = IS_SYM;
+	resWordTokens [ 22] = LOOP_SYM;
+	resWordTokens [ 23] = MOD_SYM;
+	resWordTokens [ 24] = MODULE_SYM;
+	resWordTokens [ 25] = NIL_SYM;
+	resWordTokens [ 26] = OF_SYM;
+	resWordTokens [ 27] = OR_SYM;
+	resWordTokens [ 28] = POINTER_SYM;
+	resWordTokens [ 29] = PROCEDURE_SYM;
+	resWordTokens [ 30] = RECORD_SYM;
+	resWordTokens [ 31] = REPEAT_SYM;
+	resWordTokens [ 32] = RETURN_SYM;
+	resWordTokens [ 33] = THEN_SYM;
+	resWordTokens [ 34] = TO_SYM;
+	resWordTokens [ 35] = TYPE_SYM;
+	resWordTokens [ 36] = UNTIL_SYM;
+	resWordTokens [ 37] = VAR_SYM;
+	resWordTokens [ 38] = WHILE_SYM;
+	resWordTokens [ 39] = WITH_SYM;
+//	resWordTokens [ 40] = INTEGER_SYM;
 
 }
 
@@ -866,77 +1030,77 @@ void initSymNames()
 		symNames[i][0] = "\0";
 	}
 
-	 symNames[    ARRAY_SYM][0] = "ARRAY_SYM";
-	 symNames[    BEGIN_SYM][0] = "BEGIN_SYM";
-	 symNames[    BY_SYM][0] = "BY_SYM";
-	 symNames[    CASE_SYM][0] = "CASE_SYM";
-	 symNames[    CONST_SYM][0] = "CONST_SYM";
-	 symNames[    DIV_SYM][0] = "DIV_SYM";
-	 symNames[    DO_SYM][0] = "DO_SYM";
-	 symNames[    ELSE_SYM][0] = "ELSE_SYM";
-	 symNames[    ELSIF_SYM][0] = "ELSIF_SYM";
-	 symNames[    END_SYM][0] = "END_SYM";
-	 symNames[    EXIT_SYM][0] = "EXIT_SYM";
-	 symNames[    FOR_SYM][0] = "FOR_SYM";
-	 symNames[    IF_SYM][0] = "IF_SYM";
-	 symNames[    IMPORT_SYM][0] = "IMPORT_SYM";
-	 symNames[    IN_SYM][0] = "IN_SYM";
-	 symNames[    IS_SYM][0] = "IS_SYM";
-	 symNames[    LOOP_SYM][0] = "LOOP_SYM";
-	 symNames[    MOD_SYM][0] = "MOD_SYM";
-	 symNames[    MODULE_SYM][0] = "MODULE_SYM";
-	 symNames[    NIL_SYM][0] = "NIL_SYM";
-	 symNames[    OF_SYM][0] = "OF_SYM";
-	 symNames[    OR_SYM][0] = "OR_SYM";
-	 symNames[    AND_SYM][0] = "AND_SYM";
-	 symNames[    POINTER_SYM][0] = "POINTER_SYM";
-	 symNames[    PROCEDURE_SYM][0] = "PROCEDURE_SYM";
-	 symNames[    RECORD_SYM][0] = "RECORD_SYM";
-	 symNames[    REPEAT_SYM][0] = "REPEAT_SYM";
-	 symNames[    RETURN_SYM][0] = "RETURN_SYM";
-	 symNames[    THEN_SYM][0] = "THEN_SYM";
-	 symNames[    TO_SYM][0] = "TO_SYM";
-	 symNames[    TYPE_SYM][0] = "TYPE_SYM";
-	 symNames[    UNTIL_SYM][0] = "UNTIL_SYM";
-	 symNames[    VAR_SYM][0] = "VAR_SYM";
-	 symNames[    WHILE_SYM][0] = "WHILE_SYM";
-	 symNames[    WITH_SYM][0] = "WITH_SYM";
-	 symNames[    BOOLEAN_SYM][0] = "BOOLEAN_SYM";
-	 symNames[    CHAR_SYM][0] = "CHAR_SYM";
-	 symNames[    FALSE_SYM][0] = "FALSE_SYM";
+	 symNames[     ARRAY_SYM][ 0] = "ARRAY_SYM";
+	 symNames[     BEGIN_SYM][ 0] = "BEGIN_SYM";
+	 symNames[        BY_SYM][ 0] = "BY_SYM";
+	 symNames[      CASE_SYM][ 0] = "CASE_SYM";
+	 symNames[     CONST_SYM][ 0] = "CONST_SYM";
+	 symNames[       DIV_SYM][ 0] = "DIV_SYM";
+	 symNames[        DO_SYM][ 0] = "DO_SYM";
+	 symNames[      ELSE_SYM][ 0] = "ELSE_SYM";
+	 symNames[     ELSIF_SYM][ 0] = "ELSIF_SYM";
+	 symNames[       END_SYM][ 0] = "END_SYM";
+	 symNames[      EXIT_SYM][ 0] = "EXIT_SYM";
+	 symNames[       FOR_SYM][ 0] = "FOR_SYM";
+	 symNames[        IF_SYM][ 0] = "IF_SYM";
+	 symNames[    IMPORT_SYM][ 0] = "IMPORT_SYM";
+	 symNames[        IN_SYM][ 0] = "IN_SYM";
+	 symNames[        IS_SYM][ 0] = "IS_SYM";
+	 symNames[      LOOP_SYM][ 0] = "LOOP_SYM";
+	 symNames[       MOD_SYM][ 0] = "MOD_SYM";
+	 symNames[    MODULE_SYM][ 0] = "MODULE_SYM";
+	 symNames[       NIL_SYM][ 0] = "NIL_SYM";
+	 symNames[        OF_SYM][ 0] = "OF_SYM";
+	 symNames[        OR_SYM][ 0] = "OR_SYM";
+	 symNames[       AND_SYM][ 0] = "AND_SYM";
+	 symNames[   POINTER_SYM][ 0] = "POINTER_SYM";
+	 symNames[ PROCEDURE_SYM][ 0] = "PROCEDURE_SYM";
+	 symNames[    RECORD_SYM][ 0] = "RECORD_SYM";
+	 symNames[    REPEAT_SYM][ 0] = "REPEAT_SYM";
+	 symNames[    RETURN_SYM][ 0] = "RETURN_SYM";
+	 symNames[      THEN_SYM][ 0] = "THEN_SYM";
+	 symNames[        TO_SYM][ 0] = "TO_SYM";
+	 symNames[      TYPE_SYM][ 0] = "TYPE_SYM";
+	 symNames[     UNTIL_SYM][ 0] = "UNTIL_SYM";
+	 symNames[       VAR_SYM][ 0] = "VAR_SYM";
+	 symNames[     WHILE_SYM][ 0] = "WHILE_SYM";
+	 symNames[      WITH_SYM][ 0] = "WITH_SYM";
+	 symNames[   BOOLEAN_SYM][ 0] = "BOOLEAN_SYM";
+	 symNames[      CHAR_SYM][ 0] = "CHAR_SYM";
+	 symNames[     FALSE_SYM][ 0] = "FALSE_SYM";
 	// symNames[    INTEGER_SYM][0] = "INTEGER_SYM";
-	 symNames[    NEW_SYM][0] = "NEW_SYM";
-	 symNames[    REAL_SYM][0] = "REAL_SYM";
-	 symNames[    TRUE_SYM][0] = "TRUE_SYM";
-	 symNames[	  SET_SYM][0] = "SET_SYM";
-	 symNames[    ident][0] = "IDENT";
-	 symNames[    number][0] = "NUMBER";
-	 symNames[    lparen][0] = "LPAREN";
-	 symNames[    rparen][0] = "RPAREN";
-	 symNames[    lcurly][0] = "LCURLY";
-	 symNames[    rcurly][0] = "RCURL";
-	 symNames[    plus][0] = "PLUS";
-	 symNames[    minus][0] = "MINUS";
-	 symNames[    mul][0] = "MUL";
-	 symNames[    slash][0] = "SLASH";
-	 symNames[    rbrac][0] = "RBRAC";
-	 symNames[    lbrac][0] = "LBRAC";
-	 symNames[    equal][0] = "EQUAL";
-	 symNames[    colon][0] = "COLON";
-	 symNames[    lt][0] = "LT";
-	 symNames[    lte][0] = "LTE";
-	 symNames[    gt][0] = "GT";
-	 symNames[    gte][0] = "GTE";
-	 symNames[    SEMIC][0] = "SEMIC";
-	 symNames[    null][0] = "NULL";
-	 symNames[    assign][0] = "ASSIGN";
-	 symNames[    hat][0] = "HAT";
-	 symNames[    notEqual][0] = "NOT_EQUAL";
-	 symNames[    comma][0] = "COMMA";
-	 symNames[    period][0] = "PERIOD";
-	 symNames[    string][0] = "STRING";
-	 symNames[    tilde][0] = "TILDE";
-	 symNames[    eofSym][0] = "EOF";
+	 symNames[       NEW_SYM][ 0] = "NEW_SYM";
+	 symNames[      REAL_SYM][ 0] = "REAL_SYM";
+	 symNames[      TRUE_SYM][ 0] = "TRUE_SYM";
+	 symNames[	     SET_SYM][ 0] = "SET_SYM";
+	 symNames[         ident][ 0] = "IDENT";
+	 symNames[        number][ 0] = "NUMBER";
+	 symNames[        lparen][ 0] = "LPAREN";
+	 symNames[        rparen][ 0] = "RPAREN";
+	 symNames[        lcurly][ 0] = "LCURLY";
+	 symNames[        rcurly][ 0] = "RCURL";
+	 symNames[          plus][ 0] = "PLUS";
+	 symNames[         minus][ 0] = "MINUS";
+	 symNames[           mul][ 0] = "MUL";
+	 symNames[         slash][ 0] = "SLASH";
+	 symNames[         rbrac][ 0] = "RBRAC";
+	 symNames[         lbrac][ 0] = "LBRAC";
+	 symNames[         equal][ 0] = "EQUAL";
+	 symNames[         colon][ 0] = "COLON";
+	 symNames[            lt][ 0] = "LT";
+	 symNames[           lte][ 0] = "LTE";
+	 symNames[            gt][ 0] = "GT";
+	 symNames[           gte][ 0] = "GTE";
+	 symNames[         SEMIC][ 0] = "SEMIC";
+	 symNames[          null][ 0] = "NULL";
+	 symNames[        assign][ 0] = "ASSIGN";
+	 symNames[           hat][ 0] = "HAT";
+	 symNames[      notEqual][ 0] = "NOT_EQUAL";
+	 symNames[         comma][ 0] = "COMMA";
+	 symNames[        period][ 0] = "PERIOD";
+	 symNames[        string][ 0] = "STRING";
+	 symNames[         tilde][ 0] = "TILDE";
+	 symNames[        eofSym][ 0] = "EOF";
 }
 
 //
